8de95c1e837f5d1d2b6d365b5e2bcada02684ead
DevDengChao <2325690622@qq.com>
2023-05-24 01:31:35 +0800

    chore: display vue counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Count
+    <button @click="countDown">-</button>
+    {{ count }}
+    <button @click="countUp">+</button>
+  </div>
+</template>
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countDown() {
+      this.count--;
+    },
+    countUp() {
+      this.count++;
+    },
+  }
+})
+</script>
